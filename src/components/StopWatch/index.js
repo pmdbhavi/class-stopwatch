3f1a973f6ece82b661c184deec3ca0814948192c
pmdbhavi <srirenu640@gmail.com>
2023-05-09 11:14:32 +0530

    stopwatch

--- src/components/StopWatch/index.js
+++ src/components/StopWatch/index.js
@@ -2,32 +2,22 @@ import {Component} from 'react'
 import './index.css'
 
 class StopWatch extends Component{
-    state={minutes:0,seconds:0,play:false}
+    state={minutes:0,seconds:0}
 
     toStart=()=>{
-        const {minutes,seconds,play}=this.setState
-        const timerCompleted=minutes*60===seconds
-        if(timerCompleted){
-            this.setState({seconds:0})
-        }
-        if(!play){
-            this.timerId=setInterval(()=>{
-                this.setState(prevState=>({seconds:prevState.seconds+1}))
-            },1000)
-        }
-        this.setState({play:true})
+        this.timerId=setInterval(()=>{
+            this.setState(prevState=>({seconds:prevState.seconds+1}))
+        },1000)
     }
 
     toStop=()=>{
         clearInterval(this.timerId)
-        this.setState({play:false})
     }
 
     toReset=()=>{
         clearInterval(this.timerId)
         this.setState({minutes:0})
         this.setState({seconds:0})
-        this.setState({play:false})
     }
 
     time=()=>{
